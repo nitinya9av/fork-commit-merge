1f4f6ab5417d00cc61812260fdf62c9f2b58537c
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-01-18 20:18:04 +0530

    Update sum.test.ts

--- tasks/typescript/vitest/easy/src/sum.test.ts
+++ tasks/typescript/vitest/easy/src/sum.test.ts
@@ -4,3 +4,13 @@ import sum from "./sum";
 import { describe, expect, it } from "vitest";
 
 // TODO: Create the test required in the task
+let arr: Array<number>= [3, 2, 5.4, 2, 6, 4];
+decsribe("Testing sum", function() {
+  it("Sum comparision by for loop", function() {
+    let total= 0;
+    for (let num of arr) {
+      total += num;
+    }
+    expect(sum(arr).toBe(total);
+  });
+})
