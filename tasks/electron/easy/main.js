34fee48740d1dcc7216f685e80d654fef464a660
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-01-20 11:41:48 +0530

    Update main.js

--- tasks/electron/easy/main.js
+++ tasks/electron/easy/main.js
@@ -1,17 +1,6 @@
 // Electron - Easy
 
 const { app, BrowserWindow } = require("electron");
+// TODO: Create a new BrowserWindow that displays the index.html file
 
-function createWindow() {
-    const mainWindow= new BrowserWindow({
-        width: 1920,
-        height: 1080
-    });
-    mainWindow.loadFile("index.html");
-    // TODO: Create a new BrowserWindow that displays the index.html file
-}
-
-app.on("activate", function() {
-    createWindow();
-});
-app.on("window-all-closed", function() { app.quit(); })
+app.on("ready", function() {});
